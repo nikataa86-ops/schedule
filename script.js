fc470ed62d8504e39760500af0af3329692fd674
nikataa86-ops <nika.taa86@gmail.com>
2026-06-25 21:39:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-console.log("calendar loaded");
-alert("JS動いてる！");
+const today = new Date().getDate();
+
+document.querySelectorAll(".day").forEach(day => {
+  if (Number(day.textContent) === today) {
+    day.style.background = "rgba(0, 255, 200, 0.25)";
+    day.style.boxShadow = "0 0 10px rgba(0,255,200,0.6)";
+  }
+});
