a389d1fa5f00a1cecaba737db79ca43c3bb3b234
nikataa86-ops <nika.taa86@gmail.com>
2026-06-25 21:49:15 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 const today = new Date().getDate();
-
 const calendar = document.querySelector(".calendar");
+
+calendar.innerHTML = "";
 
 for (let i = 1; i <= 31; i++) {
   const day = document.createElement("div");
   day.className = "day";
-  day.textContent = i;
 
+  const num = document.createElement("div");
+  num.textContent = i;
+
+  const dot = document.createElement("div");
+  dot.className = "dot";
+
+  day.appendChild(num);
+  day.appendChild(dot);
+
+  // 今日ハイライト
   if (i === today) {
     day.style.background = "rgba(0, 255, 200, 0.3)";
     day.style.boxShadow = "0 0 12px rgba(0,255,200,0.7)";
-    day.style.border = "1px solid rgba(0,255,200,0.8)";
   }
 
   calendar.appendChild(day);
