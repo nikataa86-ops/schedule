17a025da779a2316a451b80f555cdfe1a76128a0
nikataa86-ops <nika.taa86@gmail.com>
2026-06-29 19:24:25 +0900

    Remove HTML structure from script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-<div class="card">
-  <div class="title">📺 ラストノート 主演・樋口澄晴 役</div>
-  <div class="time">毎週木曜 22時 - 22時54分</div>
-  <div class="links">
-    <a href="https://www.google.com" class="btn" target="_blank">公式サイト</a>
-    <a href="#" class="btn">公式X</a>
-    <a href="#" class="btn">Instagram</a>
-    <a href="#" class="btn">TVer</a>
-  </div>
-</div>
+
